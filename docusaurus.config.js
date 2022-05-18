03990953b2565b7e91642b15973d517217b3b089
Yağızhan Burak Yakar <yagizhan49@protonmail.com>
2022-05-18 13:34:46 +0300

    Initial release

---
+++ docusaurus.config.js
@@ -0,0 +1,109 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'FOSSBilling Documentation',
+  url: 'https://docs.fossbilling.org',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          breadcrumbs: false,
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/fossbilling/docs/blob/master/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      announcementBar: {
+        id: 'contribute',
+        content:
+          '<b>We are looking for new people to join our team, visit <a href="/contribute">this page</a> if you are interested.</b>',
+        backgroundColor: '#4cb3d4',
+        textColor: '#fafafa',
+        isCloseable: false,
+      },
+      navbar: {
+        title: 'FOSSBilling Documentation',
+        logo: {
+          alt: 'FOSSBilling logo',
+          src: 'img/fossbilling.png',
+        },
+        items: [
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/fossbilling',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://fossbilling.org/discord',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/fossbilling',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/fossbilling/fossbilling',
+              },
+            ],
+          },
+        ],
+        copyright: `FOSSBilling`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
