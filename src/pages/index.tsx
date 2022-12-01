457512dd565efcfc066ed7c8949488cbe64d7b40
evrifaessa <hi@yagiz.dev>
2022-12-01 21:25:36 +0300

    Home page rewrite

---
+++ src/pages/index.tsx
@@ -0,0 +1,49 @@
+import ThemedImage from '@theme/ThemedImage';
+import HomepageFeatures from '@site/src/components/Homepage/Features';
+import React from 'react';
+import Link from '@docusaurus/Link';
+import clsx from 'clsx';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">
+          <ThemedImage
+            alt = "FOSSBilling logo"
+            sources = {{
+                light: '/img/logo-white.svg',
+                dark: '/img/logo-black.svg',
+            }}
+            width = "500"
+          />
+      </h1>
+        <p className="hero__subtitle">Free and open-source billing and client management solution.</p>
+        <div className={styles.buttons}>
+          <a
+            className="button button--secondary button--lg"
+            href="https://fossbilling.org/downloads/preview">
+            Download the latest preview
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  return (
+    <Layout
+      title="Homepage"
+      description="FOSSBilling is a free and open-source billing and client management solution.">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
