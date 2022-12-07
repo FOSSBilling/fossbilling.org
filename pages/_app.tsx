ac5bc2cf6bb589d06726bfb7fd4830f4330dbdd6
evrifaessa <hi@yagiz.dev>
2022-12-07 18:15:45 +0300

    +Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { ReactElement } from 'react'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
@@ -8,5 +9,10 @@ import '../style.css'
 import '../styles/globals.css'
 
 export default function Nextra({ Component,  pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
