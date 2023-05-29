c908550aa6bf8a00cee15a8f71bd5fd4353d5625
Belle Aerni <belleaerni@gmail.com>
2023-05-29 00:33:00 -0700

    Minor doc fixes and improvements

--- pages/api/releases.ts
+++ pages/api/releases.ts
@@ -5,7 +5,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<FOSSBi
   const versions = ["0.1.0", "0.1.1", "0.2.0", "0.2.1", "0.2.2",
                     "0.2.3", "0.2.4", "0.2.5", "0.2.6", "0.2.7",
                     "0.2.8", "0.2.9", "0.2.10", "0.3.0", "0.4.0",
-                    "0.4.1"]
+                    "0.4.1", "0.4.2", "0.4.3"]
 
   const latestVersion = versions[versions.length - 1];
   const outdatedVersionThreshold = "0.3.0";
