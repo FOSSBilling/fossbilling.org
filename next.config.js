580e3565bf9a432cb995c579f7f3620bce67e8dd
evrifaessa <hi@yagiz.dev>
2023-05-31 11:54:15 +0300

    +Testing Sentry

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
+const { withSentryConfig } = require('@sentry/nextjs')
+
 const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.tsx',
-    staticImage: true,
-    flexsearch: {
-      codeblocks: false
-    },
-    defaultShowCopyCode: true,
-  })
-   
-  module.exports = withNextra()
-   
-  // If you have other Next.js configurations, you can pass them as the parameter:
-  // module.exports = withNextra({ /* other next.js config */ })
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  staticImage: true,
+  flexsearch: {
+    codeblocks: false
+  },
+  defaultShowCopyCode: true,
+})
+
+module.exports = withSentryConfig(withNextra())
