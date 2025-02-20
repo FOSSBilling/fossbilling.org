264367465067d18753b17019f6c87ce60c933d81
Adam Daley <admdly@users.noreply.github.com>
2025-02-20 00:58:53 +0000

    Specify `noopener` for potentially unsafe links (#224)

--- components/ContributedBy/index.tsx
+++ components/ContributedBy/index.tsx
@@ -8,13 +8,13 @@ export function ContributedBy({ username, pullRequestNumber }) {
     return (
         <div className="flex float-right items-center p-2 ms-4 mb-4 mt-6 w-56 bg-gray-100 dark:bg-neutral-800 text-gray-600 dark:text-gray-400 rounded-md">
             <div className="inline-block">
-                <a href={userLink} target="_blank">
+                <a href={userLink} target="_blank" rel="noopener">
                     <img className="rounded-md h-10 w-10" src={avatarLink} alt={username}/>
                 </a>
             </div>
             <div className="flex ml-3">
                 <p className="text-xs">
-                    Contributed by <br /><a className="text-blue-400 hover:text-blue-500 hover:underline" href={userLink} target="_blank">{username}</a> in <a className="text-blue-400 hover:text-blue-500 hover:underline" href={pullRequestLink} target="_blank">#{pullRequestNumber}</a>.
+                    Contributed by <br /><a className="text-blue-400 hover:text-blue-500 hover:underline" href={userLink} target="_blank" rel="noopener">{username}</a> in <a className="text-blue-400 hover:text-blue-500 hover:underline" href={pullRequestLink} target="_blank" rel="noopener">#{pullRequestNumber}</a>.
                 </p>
             </div>
         </div>
