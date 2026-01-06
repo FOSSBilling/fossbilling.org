c83568d5fbd106f915ab76569b51b0a0ed97fd82
Adam Daley <admdly@users.noreply.github.com>
2026-01-06 16:35:05 +0000

    Move redirects to `next.config.mjs`

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,70 @@ const nextConfig = {
   // Use webpack instead of turbopack for now due to MDX import issues
   experimental: {
     webpackBuildWorker: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/bug-report',
+        destination: 'https://github.com/FOSSBilling/FOSSBilling/issues/new?assignees=&labels=bug&template=BUG-REPORT.yml&title=%5BBug%5D',
+        permanent: true
+      },
+      {
+        source: '/discord',
+        destination: 'https://discord.com/invite/bVjMZSgtbY',
+        permanent: true
+      },
+      {
+        source: '/donate',
+        destination: 'https://github.com/sponsors/FOSSBilling',
+        permanent: true
+      },
+      {
+        source: '/feature-request',
+        destination: 'https://github.com/FOSSBilling/FOSSBilling/issues/new?assignees=&labels=feature+request&template=feature_request.md&title=%5BFeature+Request%5D',
+        permanent: true
+      },
+      {
+        source: '/downloads/stable',
+        destination: 'https://github.com/FOSSBilling/FOSSBilling/releases/latest/download/FOSSBilling.zip',
+        permanent: true
+      },
+      {
+        source: '/downloads/preview',
+        destination: 'https://download.fossbilling.org/FOSSBilling-preview.zip',
+        permanent: true
+      },
+      {
+        source: '/api/central-alerts/:query*',
+        destination: 'https://api.fossbilling.net/central-alerts/v1/:query*',
+        permanent: true
+      },
+      {
+        source: '/docs/contribution-handbook/:slug*',
+        destination: '/docs/developing-fossbilling/:slug*',
+        permanent: true
+      },
+      {
+        source: '/docs/getting-started/apache',
+        destination: '/docs/getting-started/installation',
+        permanent: true
+      },
+      {
+        source: '/docs/getting-started/shared',
+        destination: '/docs/getting-started/installation',
+        permanent: true
+      },
+      {
+        source: '/docs/getting-started/nginx',
+        destination: '/docs/getting-started/installation',
+        permanent: true
+      },
+      {
+        source: '/docs/getting-started/softaculous',
+        destination: '/docs/getting-started/installation',
+        permanent: true
+      }
+    ]
   }
 }
 
