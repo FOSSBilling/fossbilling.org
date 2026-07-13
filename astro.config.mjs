2e3e0c69636e1b0a05b035a272f136388a3ce47c
Adam Daley <admdly@users.noreply.github.com>
2026-07-13 20:42:56 +0100

    Enforce trailing slash URLs sitewide

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fossbilling.org',
+  trailingSlash: 'always',
   output: 'static',
   adapter: cloudflare({ prerenderEnvironment: 'node' }),
   integrations: [sitemap()],
